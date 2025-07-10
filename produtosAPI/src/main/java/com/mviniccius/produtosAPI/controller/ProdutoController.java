4289895686e05b049d681e9b4397eb6c253c2391
mviniccius <marcus.viniccius@hotmail.com>
2025-07-10 18:25:59 -0300

    crud de produto

--- produtosAPI/src/main/java/com/mviniccius/produtosAPI/controller/ProdutoController.java
+++ produtosAPI/src/main/java/com/mviniccius/produtosAPI/controller/ProdutoController.java
@@ -1,19 +1,47 @@
 package com.mviniccius.produtosAPI.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.mviniccius.produtosAPI.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import com.mviniccius.produtosAPI.model.Produto;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
 
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
 
     @PostMapping
-    public void salvar(@RequestBody Produto produto){
+    public Produto salvar(@RequestBody Produto produto){
         System.out.println("Produto recebido com sucesso: " +  produto.getNome());
+        var id = UUID.randomUUID().toString();
+        produto.setId(id);
+        produtoRepository.save(produto);
+        return produto;
+    }
+    @GetMapping("/{id}")
+    public Produto obterProdutoId(@PathVariable("id") String id){
+        System.out.println("Produto encontrato");
+        return produtoRepository.findById(id).orElse( null);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable String id){
+        produtoRepository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public void atualizarProduto(@PathVariable("id") String id, @RequestBody Produto produto){
+        produto.setId(id);
+        produtoRepository.save(produto);
+
     }
 }
--- produtosAPI/src/main/java/com/mviniccius/produtosAPI/model/Produto.java
+++ produtosAPI/src/main/java/com/mviniccius/produtosAPI/model/Produto.java
@@ -1,10 +1,22 @@
 package com.mviniccius.produtosAPI.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table //mapar qual tabela do banco de dados
 public class Produto {
 
+    @Id
+    @Column (name ="id")//serve para mapear colunas no banco de dados
     private String id;
+    @Column
     private String nome;
+    @Column
     private String descricao;
+    @Column
     private double preco;
 
     public String getId(){
