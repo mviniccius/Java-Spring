377903b9c1341f8a7b92f95b4e5e20f49ce87987
mviniccius <marcus.viniccius@hotmail.com>
2025-08-01 15:45:03 -0300

    configurando banco de dados com docker

---
+++ libaryapi/src/test/java/com/mviniccius/libaryapi/LibaryapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mviniccius.libaryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibaryapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
